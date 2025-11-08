109979fa5ceb273e4e8a1bf8f590d217ca3a51b5
Alef-Daniel-Mello <alefdanieldemellocoutinho@gmail.com>
2025-11-08 18:17:35 +0000

    Desafio Aventureiro

--- war.c
+++ war.c
@@ -29,89 +29,67 @@ int Territorio_Atual = 0;
 #define Cor_Max 20
 #define Tropa_Max 20
 int Opcao;
-// --- Estrutura de Dados --- 
-struct Territorio{
+// --- Estrutura de Dados -----------------------------
+  struct Territorio{
 
+   char Dono[Nome_Max];
    char Nome[Nome_Max];
    char Cor[Cor_Max];
    int Tropas;
-
+   
 };
-    struct Territorio Terra[Territorio_Max];
 
-// --- Protótipos das Funções ---
+
+// --- Protótipos das Funções ----------------------------
 void LimparBuffer(){
     int c;
     while ((c = getchar()) !='\n' && c != EOF);
 }
 
-void Cadastrar()
+int AtacarTerritorio()
 {
-    
-    if(Territorio_Atual < Territorio_Max)
-    {
-    printf("\n\n=======================================\n");
-    printf("Cadastro de território\n");
-    printf("=======================================\n");
-    // Cadastrar Nome
-    printf("\nDigite o nome do território %d :\n",Territorio_Atual+1);
-    fgets(Terra[Territorio_Atual].Nome,Nome_Max,stdin);  
-    // Cadastrar Cor
-    printf("\nDigite a cor do território %d :\n",Territorio_Atual+1);
-    fgets(Terra[Territorio_Atual].Cor,Cor_Max,stdin);  
-    // Cadastrar Tropas
-    printf("\nDigite a quantidade de tropas do território %d :\n",Territorio_Atual+1);
-    scanf("%d",&Terra[Territorio_Atual].Tropas);  
-    LimparBuffer();
-    Territorio_Atual++;
-    printf("\n\nContinuar cadastrando?\n");
-    printf("1 - Sim  /  2 - Não\n");
-    int resposta = 0;
-    scanf("%d",&resposta);
-    LimparBuffer();
-    switch (resposta)
-    {
-    case 1:
-        Cadastrar();
-    default:
-        Menu();
-    }
-    Menu();
-    }
-    else{
-        printf("\nNúmero máximo de territórios atingidos, retornando ao Menu\n");
-        Menu();
-    }
+   int ForçaAtacante;
+   int ForçaDefensor;
+   
+   ForçaAtacante = rand();
+   printf("\n Força do atacante é : %d",ForçaAtacante);
+   ForçaDefensor = rand();
+   printf("\n Força do defensor é : %d",ForçaDefensor);
+
+   if(ForçaAtacante > ForçaDefensor){
+    return 0;
+   }
+   else if(ForçaAtacante < ForçaDefensor){
+    return 1;
+   }
+   else if(ForçaAtacante == ForçaDefensor){
+    return 3;
+   }
 }
+// Declarações antecipadas de todas as funções que serão usadas no programa, organizadas por categoria.
+// Funções de setup e gerenciamento de memória:
+// Funções de interface com o usuário:
+// Funções de lógica principal do jogo:
+// Função utilitária:
 
-void Exibir(){
+// --- Função Principal (main) ----------------------------
+int main() {
+
+    struct Territorio *Terra;
+
+    Terra = (struct Territorio*) calloc(Territorio_Max,sizeof(struct Territorio));
+    int (*Atacar)() = AtacarTerritorio;
     
-    
-    printf("\n\n=======================================\n");
-    printf("Exibição de territórios\n");
-    printf("=======================================\n");
-    if(Territorio_Atual == 0){
-        printf("Não há territórios cadastrados");
-        Menu();
+    if(Terra == NULL)
+    {
+        printf("Erro ao alocar memória");
+        return 1;
     }
-    else{
-        for (int i = 0; i < Territorio_Atual; i++)
-        {
-             printf("%d -------------------------------------\n", i+1);
-             printf("Território %d :\n", i+1);
-             printf("Nome : %s",Terra[i].Nome);
-             printf("Cor : %s",Terra[i].Cor);
-             printf("Quantidade de tropas : %d\n",Terra[i].Tropas);
-             printf("-------------------------------------\n\n");
 
+    do
+    {
 
-        }
-        Menu();
-    }
-}
-
-void Menu(){
-    Opcao = 0;
+    // Menu Inicial -----------------------------------------------------------
     printf("========================================\n");
     printf("         Desafio War - Novato\n");
     printf("========================================\n");
@@ -121,32 +99,132 @@ void Menu(){
     printf("Selecione uma opção :\n");
     printf("1 - Cadastrar territórios\n");
     printf("2 - Exibir territórios\n");
+    printf("3 - Atacar territórios\n");
     printf("0 - Sair\n");
 
     scanf("%d",&Opcao);
     LimparBuffer();
+   
     switch (Opcao)
     {
+    // Cadastro ---------------------------------------------------------------
     case 1:
-        Cadastrar();
-    case 2:
-        Exibir();
-    case 0:
-        printf("\nPrograma encerrado\n");
-        break;
+        if(Territorio_Atual < Territorio_Max)
+    {
+    printf("\n\n=======================================\n");
+    printf("Cadastro de território\n");
+    printf("=======================================\n");
+    // Cadastrar Nome
+    printf("\nDigite o nome do território %d :\n",Territorio_Atual+1);
+    fgets(Terra[Territorio_Atual].Nome,Nome_Max,stdin); 
+    strcpy(Terra[Territorio_Atual].Dono, Terra[Territorio_Atual].Nome);
+  
+    // Cadastrar Cor
+    printf("\nDigite a cor do território %d :\n",Territorio_Atual+1);
+    fgets(Terra[Territorio_Atual].Cor,Cor_Max,stdin);  
+    // Cadastrar Tropas
+    printf("\nDigite a quantidade de tropas do território %d :\n",Territorio_Atual+1);
+    scanf("%d",&Terra[Territorio_Atual].Tropas);  
+    LimparBuffer();
+    Territorio_Atual++;
     }
-}
-// Declarações antecipadas de todas as funções que serão usadas no programa, organizadas por categoria.
-// Funções de setup e gerenciamento de memória:
-// Funções de interface com o usuário:
-// Funções de lógica principal do jogo:
-// Função utilitária:
+        else{
+        printf("\nNúmero máximo de territórios atingidos, retornando ao Menu\n");
+                
 
-// --- Função Principal (main) ---
-// Função principal que orquestra o fluxo do jogo, chamando as outras funções em ordem.
-int main() {
-    Menu();
+    }
+        break;
+    case 2:
+    // Exibição -----------------------------------------------------------------
+       if(Territorio_Atual == 0){
+        printf("Não há territórios cadastrados");
+        
+    }
+       else{
 
+        printf("\n\n=======================================\n");
+    printf("Exibição de territórios\n");
+    printf("=======================================\n");
+        for (int i = 0; i < Territorio_Atual; i++)
+        {
+             printf("%d -------------------------------------\n", i+1);
+             printf("Dono %d :\n", Terra[i].Dono);
+             printf("Nome : %s",Terra[i].Nome);
+             printf("Cor : %s",Terra[i].Cor);
+             printf("Quantidade de tropas : %d\n",Terra[i].Tropas);
+             printf("-------------------------------------\n\n");
+
+
+        }
+
+    }
+       break;
+    case 3:
+    // Guerra ---------------------------------------------------------------------
+      if (Territorio_Atual >= 2)
+      {
+        int Atacante = 0;
+        int Defensor = 0;
+       printf("\n\n=======================================\n");
+       printf("Ataque de territórios!\n");
+       printf("=======================================\n");
+       printf("\nEscolha o Território atacante :\n");
+       scanf("%d",&Atacante);
+       LimparBuffer();
+       printf("\nEscolha o Território defensor :\n");
+       scanf("%d",&Defensor);
+       LimparBuffer();
+
+       int Vencedor = Atacar();
+       // Atacante venceu ----------------------
+       if(Vencedor == 0)
+       {
+       printf("\n\n=== O vencedor é o território : %d! ===\n",Atacante);
+       printf("Reduzindo 1 tropa do perdedor...\n\n");
+       Terra[Defensor-1].Tropas--;
+       }
+       // Defensor venceu ---------------
+       else if (Vencedor == 1)
+       {
+        printf("\n\n=== O vencedor é o território : %d! ===\n",Defensor);
+       printf("Reduzindo 1 tropa do perdedor...\n\n");
+       Terra[Atacante-1].Tropas--;
+
+       }
+       // Empate -------------------------
+       else
+       {
+        printf("\n\n=== Houve um Empate! ===\n");
+        printf("Vitória pro atacante, Reduzindo 1 tropa do perdedor...\n\n");
+       Terra[Defensor-1].Tropas--;
+       }
+       
+       // Resultados -----------------------------------
+       if(Terra[Atacante-1].Tropas == 0)
+       {
+        printf("\nAtacante perdeu suas tropas, agora pertence ao Defensor!\n");
+        strcpy(Terra[Atacante-1].Dono, Terra[Defensor-1].Nome);
+       }
+       else if(Terra[Atacante-1].Tropas == 0)
+       {
+        printf("\nDefensor perdeu suas tropas, agora pertence ao Atacante!\n");
+        strcpy(Terra[Defensor-1].Dono, Terra[Atacante-1].Nome);
+       }
+      } 
+      else
+      {
+        printf("\nTerritórios insuficientes!\n");
+      } 
+      break;
+    case 0:
+        break;
+
+    }
+    } while (Opcao != 0);
+    
+    free(Terra);
+    free(Atacar);
+    return 0;
     
 
     // 1. Configuração Inicial (Setup):
